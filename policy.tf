a55237b4d6f62a1ccfbfb745869ddabf536833ae
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 12:05:51 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,9 +1,1 @@
-resource "azurerm_subscription_policy_assignment" "policyassignment" {
-  for_each = { for p in csvdecode(file("${path.module}/Policy.csv")): p.displayname => p }
-  
-  name                  = substr(replace(each.key, " ", "-"), 0, 24)
-  display_name          = each.value.displayname
-  policy_definition_id  = each.value.policyid
- subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
-}
 
