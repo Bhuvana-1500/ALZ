dea71059596b5e4721fa503babeae0561c1b925a
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 12:00:50 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,9 +1,9 @@
-resource "azurerm_management_group_policy_assignment" "policyassignment" {
+resource "azurerm_subscription_policy_assignment" "policyassignment" {
   for_each = { for p in csvdecode(file("${path.module}/Policy.csv")): p.displayname => p }
-  provider= azurerm.connectivity
+  
   name                  = substr(replace(each.key, " ", "-"), 0, 24)
   display_name          = each.value.displayname
   policy_definition_id  = each.value.policyid
-  management_group_id   = azurerm_management_group.root.id
+ subscription_id = "13ba43d9-3859-4c70-9f8d-182debaa038b"
 }
 
