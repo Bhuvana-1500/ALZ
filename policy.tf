1c147ebf0b13f70b07e074b995163df89f4286a1
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 11:43:14 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,6 +1,6 @@
 resource "azurerm_management_group_policy_assignment" "policyassignment" {
   for_each = { for p in csvdecode(file("${path.module}/Policy.csv")): p.displayname => p }
-  
+  provider= azurerm.connectivity
   name                  = substr(replace(each.key, " ", "-"), 0, 24)
   display_name          = each.value.displayname
   policy_definition_id  = each.value.policyid
