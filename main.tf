c82154e0182d2fd3ce1e7d80d667c91f1d31d35e
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 12:15:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,25 @@ resource "azurerm_resource_group" "rg1" {
   location = local.resource_groups1[each.key].location
 }
 
+# Create the virtual networks and subnets
+resource "azurerm_virtual_network" "vnet" {
+  provider = azurerm.connectivity
+  for_each            = local.resource_groups
+  name                = each.value.vnet_name  # Access the first virtual network name in the list
+  location            = each.value.location
+  resource_group_name = each.value.name
+  address_space       = each.value.address_space
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+provider = azurerm.management
+  for_each            = local.resource_groups1
+  name                = each.value.vnet_name  # Access the single virtual network name as a string
+  location            = each.value.location
+  resource_group_name = each.value.name
+  address_space       = each.value.address_space
+}
+
 
 
 
