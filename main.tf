880f0b6402d44bd88f9605d097cb71cc245bd204
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-05 11:57:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# Create root management group
-resource "azurerm_management_group" "root" {
-  name                       = local.root_management_group.name
-  display_name               = local.root_management_group.display_name
-  parent_management_group_id = local.root_management_group.parent_management_group_id
-  subscription_ids           = local.root_management_group.subscription_ids
-}
-
 resource "azurerm_resource_group" "rg" {
   provider = azurerm.connectivity
   for_each = local.resource_groups
