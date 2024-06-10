a2343e2a89eccba49ac89e4175ef6af9d5c6fec4
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-10 12:14:02 +0530

    Update policy.tf

--- policy.tf
+++ policy.tf
@@ -1,1 +1,8 @@
-
+resource "azurerm_resource_group_policy_assignment" "policyassignment" {
+  for_each = { for p in csvdecode(file("${path.module}/Policy.csv")): p.displayname => p }
+  
+  name                  = substr(replace(each.key, " ", "-"), 0, 24)
+  display_name          = each.value.displayname
+  policy_definition_id  = each.value.policyid
+  resource_group_id     = "/subscriptions/13ba43d9-3859-4c70-9f8d-182debaa038b/resourceGroups/rg-hub-uks" 
+}
