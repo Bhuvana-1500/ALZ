6b298160c946bea8dd2147bc3ce27b3effad3689
Bhuvana-1500 <bhuvanamungara15@gmail.com>
2024-06-10 14:31:07 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
     name                       = "Tenant_Root_Group1"
     display_name               = "Tenant Root Group1"
     parent_management_group_id = null  # Since it's the root group
-    subscription_ids           = ["13ba43d9-3859-4c70-9f8d-182debaa038b","bd73b938-1dbc-4f3a-84ec-51e240f8bd64"]    # No subscriptions at root level
+    subscription_ids           = ["13ba43d9-3859-4c70-9f8d-182debaa038b","5e0ec067-7409-4ede-ae47-ed0926bad2ee"]    # No subscriptions at root level
   }
 }
 
